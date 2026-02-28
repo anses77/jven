63b7e8735a827f6824199aa73a4c87448473295c
anses77 <jobsvacancysg695@gmail.com>
2026-03-01 02:00:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $currentState = $_SESSION["state"];
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <style>
         :root {
@@ -481,3 +482,4 @@ $currentState = $_SESSION["state"];
 
 
 </html>
+
